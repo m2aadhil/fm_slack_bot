28f5d81c4c72c9e07780d9e938f1c84141c922de
Aadhil Musthaq <amt@container-xchange.com>
2022-08-07 16:03:22 +0530

    add express

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
-const { WebClient } = require("@slack/web-api");
+const { createServer } = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 const { createEventAdapter } = require("@slack/events-api");
-
+const port = process.env.PORT || 3000;
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+const { WebClient } = require("@slack/web-api");
 // Create a new instance of the WebClient class with the token read from your environment variable
 const web = new WebClient(process.env.SLACK_TOKEN);
-const slackEvents = createEventAdapter(process.env.SLACK_TOKEN);
-// The current date
-const currentTime = new Date().toTimeString();
-
-var cron = require("node-cron");
+const cron = require("node-cron");
 const financeTeam = require("./members");
 
-// Read the port from the environment variables, fallback to 3000 default.
-const port = process.env.PORT || 3000;
+// Create an express application
+const app = express();
+app.use("/my/path", slackEvents.requestListener());
 
-const postMessage = async() => {
-    const user = financeTeam.getNextMonitoringPerson();
-    try{
-        await web.chat.postMessage({
-            channel: "#finance-test-bot",
-            link_names: 1,
-            text: `Hello @${user.userId}! You are the monitoring person for this week. Good Luck!`,
-          });
-          console.log("Message posted!");
-    } catch (error) {
+app.get('/', (req, res) => {
+  res.send('app works..!');
+});
+
+// Example: If you're using a body parser, always put it after the event adapter in the middleware stack
+app.use(bodyParser());
+
+const postMessage = async () => {
+  const user = financeTeam.getNextMonitoringPerson();
+  try {
+    await web.chat.postMessage({
+      channel: "#finance-test-bot",
+      link_names: 1,
+      text: `Hello @${user.userId}! It seems you are the monitoring person for this week. Good Luck! :))`,
+    });
+    console.log("Message posted!");
+  } catch (error) {
     console.log(error);
   }
-
-}
+};
 
 (async () => {
   // Start the built-in server
-  const server = await slackEvents.start(port);
+  // Initialize a server for the express app - you can skip this and the rest if you prefer to use app.listen()
+  const server = createServer(app);
+  server.listen(port, () => {
+    // Log a message when the server is ready
+    console.log(`Listening for events on ${server.address().port}`);
+  });
 
-    cron.schedule("* * * * *", () => {
-        postMessage();
-        console.log("running a task every minute");
-    });
-
-  // Log a message when the server is ready
-  console.log(`Listening for events on ${server.address().port}`);
+  //0 8 * * MON
+  cron.schedule("* * * * *", () => {
+    postMessage();
+    console.log("running a task every minute");
+  });
 })();
