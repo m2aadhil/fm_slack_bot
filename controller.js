6374b347bdbc0b889d66fc1af04f65269989c033
Aadhil Musthaq <amt@container-xchange.com>
2022-08-10 11:22:17 +0530

    add member

--- controller.js
+++ controller.js
@@ -4,6 +4,10 @@ const app = express();
 const webChat = require("./services/webChat");
 const financeTeam = require("./services/members");
 
+app.use(bodyParser());
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 //routes
 app.get('/', (req, res) => {
   res.send('app works..!');
@@ -20,6 +24,12 @@ app.post('/getschedule', (req, res) => {
   res.json({"blocks": schedule});
 });
 
-app.use(bodyParser());
+app.post('/addmember', (req, res) => {
+  const text = req.body.text;
+  console.log(text);
+  const schedule = financeTeam.getFullSchedule();
+  res.end("user added successfully");
+});
+
 
 exports.app = app;
