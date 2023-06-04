1de7d24354c71efa8bc902c80629c5485e09b024
aadhil <amt@container-xchange.com>
2023-06-05 01:36:44 +0200

    enable job

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const { createServer } = require("http");
 const controller = require("./api/controller");
+const cron = require('node-cron');
+const job = require('./services/jobs');
 
 const port = process.env.PORT || 3000;
 
+const scheduledTime = '0 10 * * 1';
+
+cron.schedule(scheduledTime, function() {
+  job.runMonitoringReminder();
+});
 
 (async () => {
   const server = createServer(controller.app);
+
   server.listen(port, () => {
     console.log(`Starting the server on ${server.address().port}`);
   });
